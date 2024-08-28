819cf9e9a7608e2ace01f7ccf7681e7f99ffbdc7
mariia <mariia@mail.uni-paderborn.de>
2024-08-28 16:02:51 +0200

    complited tasks a,b,c

--- js/app.js
+++ js/app.js
@@ -27,3 +27,80 @@ function createSortRemove() {
 function compare(a, b) {
     return a - b;
 }
+
+function givenArray() {
+    const givenArr = [16, -37, 54, -4, 72, -56, 47, 4, -16, 25, -37, 46, 4, -51, 27, -63, 4, -54, 76, -4, 12, -35, 4, 47];
+    function sumOfPositiveElements() {
+        let sum = 0;
+        for (let i = 0; i < givenArr.length; i++) {
+            if (givenArr[i] > 0) {
+                sum += givenArr[i];
+            }
+        }
+        return sum;
+    }
+
+    function countOfPositiveElements() {
+        let amount = 0;
+        for (let i = 0; i < givenArr.length; i++) {
+            if (givenArr[i] > 0) {
+                amount++;
+            }
+        }
+        return amount;
+    }
+
+    const resultOfSum = sumOfPositiveElements();
+    const resultOfCount = countOfPositiveElements();
+    document.getElementById('item_a').innerHTML = `The sum of positives elements: ${resultOfSum}. Count of positives elements: ${resultOfCount}.`;
+
+    function minimumElementAndItsIndex() {
+        let min = givenArr[0];
+        let indexOfMin = 0;
+        for (let i = 0; i < givenArr.length; i++) {
+            if (givenArr[i] < min) {
+                min = givenArr[i];
+                indexOfMin = i;
+            }
+        }
+        return {min, indexOfMin};
+    }
+    const resultOfMin = minimumElementAndItsIndex();
+    document.getElementById('item_b').innerHTML = `The minimum element: ${resultOfMin.min}. Its index: ${resultOfMin.indexOfMin}.`;
+
+    function maxElementAndItsIndex() {
+        let max = givenArr[0];
+        let indexOfMax = 0;
+        for (let i = 0; i < givenArr.length; i++) {
+            if (givenArr[i] > max) {
+                max = givenArr[i];
+                indexOfMax = i;
+            }
+        }
+        return {max, indexOfMax};
+    }
+    const resultOfMax = maxElementAndItsIndex();
+    document.getElementById('item_c').innerHTML = `The maximum element: ${resultOfMax.max}. Its index: ${resultOfMax.indexOfMax}.`;
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
